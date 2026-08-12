a68c68b4cb279f6650548a8bc376bcbbc88d581d
Alex Conrey <alex.conrey@k2space.com>
2026-08-12 08:57:40 -0500

    style: cargo fmt

--- src/aws_sign.rs
+++ src/aws_sign.rs
@@ -151,9 +151,18 @@ fn parse_http_date(date: &str) -> Option<String> {
     }
     let day: u32 = parts[1].parse().ok()?;
     let month: u32 = match parts[2] {
-        "Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" => 4,
-        "May" => 5, "Jun" => 6, "Jul" => 7, "Aug" => 8,
-        "Sep" => 9, "Oct" => 10, "Nov" => 11, "Dec" => 12,
+        "Jan" => 1,
+        "Feb" => 2,
+        "Mar" => 3,
+        "Apr" => 4,
+        "May" => 5,
+        "Jun" => 6,
+        "Jul" => 7,
+        "Aug" => 8,
+        "Sep" => 9,
+        "Oct" => 10,
+        "Nov" => 11,
+        "Dec" => 12,
         _ => return None,
     };
     let year: u32 = parts[3].parse().ok()?;
